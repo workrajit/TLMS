d3ad29ff116e55b18e96719064462a98e5c22a25
Yash <randomyashh@gmail.com>
2025-02-03 22:25:34 +0530

    modified root layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased tracking-tighter`}
         >
